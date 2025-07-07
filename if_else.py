f1650624e1533bd50e2199d3000d051607b68152
SUJOY GARAI <sujoycode999@gmail.com>
2025-07-07 12:53:30 +0530

    Add Java and Python Hello World programs

--- if_else.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter a number: ")
-if num == 2:
-  print("the number is 2")
-else:
-  print("the number is not 2")
--- main.py
+++
@@ -1,3 +0,0 @@
-text = "hello world!"
-for i in range(1,10,1):
-  print(text)
