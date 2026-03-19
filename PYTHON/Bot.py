45e6d65a2d5130b42bb4f96770bf1b218dfcfc57
SUJOY GARAI <160879898+Sujoy0001@users.noreply.github.com>
2026-03-20 00:20:15 +0530

    Update Bot.py

--- PYTHON/Bot.py
+++ PYTHON/Bot.py
@@ -2,7 +2,7 @@ bsnshprint("this is a bot file")
 for i in range(1,10,1):
   print("SUJOY")
 print("Aot in on")
-print("Nothing....")
+print("Nothing....")bslshvnf
 print("date overload")
 print("5")
 print("hsgg")
