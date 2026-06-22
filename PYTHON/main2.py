a45710ec1f5f12d486946afadfb693d85dc03add
SUJOY GARAI <160879898+Sujoy0001@users.noreply.github.com>
2026-06-22 18:27:40 +0530

    Update main2.py

--- PYTHON/main2.py
+++ PYTHON/main2.py
@@ -1,2 +1,24 @@
-for i in range(0,10,1):
-  print("None")
+from collections import deque
+
+def ladderLength(beginWord, endWord, wordList):
+    if endWord not in wordList:
+        return 0
+
+    wordSet = set(wordList)
+    queue = deque([(beginWord, 1)])
+
+    while queue:
+        word, steps = queue.popleft()
+
+        if word == endWord:
+            return steps
+
+        for i in range(len(word)):
+            for c in "abcdefghijklmnopqrstuvwxyz":
+                nextWord = word[:i] + c + word[i+1:]
+
+                if nextWord in wordSet:
+                    wordSet.remove(nextWord)
+                    queue.append((nextWord, steps + 1))
+
+    return 0
