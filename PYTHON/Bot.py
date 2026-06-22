4462e78c22741d61ecfd49e17560e4eec4cb161d
SUJOY GARAI <160879898+Sujoy0001@users.noreply.github.com>
2026-06-22 18:26:46 +0530

    Implement trap function for water calculation
    Replaced bot functionality with a trap water calculation function.

--- PYTHON/Bot.py
+++ PYTHON/Bot.py
@@ -1,9 +1,23 @@
-gfgjihccffnsjjsbsnshprint("this is a bot file")
-for i in range(1,10,1):
-  print("SUJOY")
-print("Aot in on")
-print("Nothing....")bslshvnf
-print("date overload")
-print("5")
-print("hsgg")
-.v
+def trap(height):
+    if not height:
+        return 0
+
+    l, r = 0, len(height) - 1
+    leftMax = rightMax = 0
+    res = 0
+
+    while l < r:
+        if height[l] < height[r]:
+            if height[l] >= leftMax:
+                leftMax = height[l]
+            else:
+                res += leftMax - height[l]
+            l += 1
+        else:
+            if height[r] >= rightMax:
+                rightMax = height[r]
+            else:
+                res += rightMax - height[r]
+            r -= 1
+
+    return res
