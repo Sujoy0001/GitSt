d2789a8b92a35c124c27820e54245ed29522273b
SUJOY GARAI <160879898+Sujoy0001@users.noreply.github.com>
2026-06-23 23:37:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Data types
+const name = "Alex";          // String
+const age = 25;               // Number
+const isDeveloper = true;     // Boolean
+let currentProject = null;    // Null (empty)
+let undefinedValue;           // Undefined
+
+// Objects (Key-value pairs)
+const user = {
+  name: "Alex",
+  age: 25
+};
+
+// Arrays (Ordered lists)
+const programmingLanguages = ["JavaScript", "Python", "C++"];
